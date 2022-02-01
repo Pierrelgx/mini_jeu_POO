c63a86304e4876e0d15f52f8a36a8096a072c188
Pierrelgx <97983718+Pierrelgx@users.noreply.github.com>
2022-02-01 09:42:40 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
-# lignes très pratiques qui appellent les gems du Gemfile. On verra plus tard comment s'en servir ;) - ça évite juste les "require" partout
+#appellent les gems du Gemfile
 require 'bundler'
 Bundler.require
 
-# lignes qui appellent les fichiers lib/user.rb et lib/event.rb
+# lignes qui appellent les fichiers lib/game.rb et lib/eventplayer.rb
 # comme ça, tu peux faire User.new dans ce fichier d'application. Top.
 require_relative 'lib/game'
 require_relative 'lib/player'
